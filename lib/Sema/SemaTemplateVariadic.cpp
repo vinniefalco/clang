fdda8c1c07f834f244173d7aac074dab703f6b8c
Eric Fiselier <eric@efcs.ca>
2018-03-31 19:46:44 -0600

    fix bug

--- lib/Sema/SemaTemplateVariadic.cpp
+++ lib/Sema/SemaTemplateVariadic.cpp
@@ -803,7 +803,6 @@ bool Sema::containsUnexpandedParameterPacks(Declarator &D) {
   switch (DS.getTypeSpecType()) {
   case TST_typename:
   case TST_typeofType:
-  case TST_underlyingType:
   case TST_atomic: {
     QualType T = DS.getRepAsType().get();
     if (!T.isNull() && T->containsUnexpandedParameterPack())
@@ -811,6 +810,7 @@ bool Sema::containsUnexpandedParameterPacks(Declarator &D) {
     break;
   }
 
+  case TST_underlyingType:
   case TST_rawInvocationType: {
     ArrayRef<ParsedType> Args = DS.getRepAsTypeList();
     for (auto PT : Args) {
