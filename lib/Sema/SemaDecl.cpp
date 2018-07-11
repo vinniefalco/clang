5c88574722dd4bfd4323f84982db12524871983b
Eric Fiselier <eric@efcs.ca>
2018-07-10 18:07:17 -0600

    changes

--- lib/Sema/SemaDecl.cpp
+++ lib/Sema/SemaDecl.cpp
@@ -11551,22 +11551,31 @@ void Sema::CheckCompleteVariableDeclaration(VarDecl *var) {
 
     // Don't emit further diagnostics about constexpr globals since they
     // were just diagnosed.
-    if (!var->isConstexpr() && GlobalStorage && var->hasAttr<ConstInitAttr>()) {
-
-      // Use strict checking in C++11 onwards, otherwise simply check the
-      // compiler can emit it as a constant expression.
-      bool DiagErr = getLangOpts().CPlusPlus11 ? !var->checkInitIsICE() : !checkConstInit();
-
-      if (DiagErr) {
+    if (!var->isConstexpr() && GlobalStorage &&
+            var->hasAttr<ConstInitAttr>()) {
+      // FIXME: Need strict checking in C++03 here.
+      SmallVector<PartialDiagnosticAt, 8> Notes;
+      bool HasError = [&]() {
+        if (getLangOpts().CPlusPlus11) {
+          // checkInitIsICE() is overly strict, but is likely already cached.
+          // If that say's we're OK, we don't need to go any further.
+          if (var->checkInitIsICE())
+            return true;
+          // Otherwise, properly evaluate the initializer within the context
+          // of the specified var, allowing for zero-initialization.
+          APValue Value;
+          return !Init->EvaluateAsInitializer(Value, getASTContext(), var, Notes);
+        }
+        // FIXME: Use non-strict checking in C++03.
+        return !checkConstInit();
+      }();
+      if (HasError) {
         auto attr = var->getAttr<ConstInitAttr>();
         Diag(var->getLocation(), diag::err_constinit_attr_failed)
-            << Init->getSourceRange();
+          << Init->getSourceRange();
         Diag(attr->getLocation(), diag::note_declared_constinit_attr_here)
-            << attr << attr->getRange();
+          << attr << attr->getRange();
         if (getLangOpts().CPlusPlus11) {
-          APValue Value;
-          SmallVector<PartialDiagnosticAt, 8> Notes;
-          Init->EvaluateAsInitializer(Value, getASTContext(), var, Notes);
           for (auto &it : Notes)
             Diag(it.first, it.second);
         } else {
